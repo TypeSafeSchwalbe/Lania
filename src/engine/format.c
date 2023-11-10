e90279294cd0ed48982371c384a8ff6676aae86e
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2023-11-10 18:47:12 +0200

    Fixed `format`-function possibly not working with some compilers

--- src/engine/format.c
+++ src/engine/format.c
@@ -6,8 +6,10 @@ char* format(AllocHandler alloc_handler, const char* fmt, ...) {
     va_list argp;
     va_start(argp, fmt);
     size_t result_size = vsnprintf(NULL, 0, fmt, argp);
+    va_end(argp);
     va_start(argp, fmt);
     char* result = (char*) (alloc_handler)(sizeof(char) * result_size + 1);
     vsprintf(result, fmt, argp);
+    va_end(argp);
     return result;
 }
